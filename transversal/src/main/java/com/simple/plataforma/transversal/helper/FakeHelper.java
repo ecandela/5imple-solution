f46c7385f84572674c52bf28758c9d50fa199dc1
ecandela <erik.candela.rojas@gmail.com>
2015-09-09 23:18:14 -0500

    test

---
+++ transversal/src/main/java/com/simple/plataforma/transversal/helper/FakeHelper.java
@@ -0,0 +1,7 @@
+package com.simple.plataforma.transversal.helper;
+
+/**
+ * Created by ecandela on 09/09/2015.
+ */
+public class FakeHelper {
+}
---
+++ transversal/src/main/java/com/simple/plataforma/transversal/mapa/FakeMapa.java
@@ -0,0 +1,7 @@
+package com.simple.plataforma.transversal.mapa;
+
+/**
+ * Created by ecandela on 09/09/2015.
+ */
+public class FakeMapa {
+}
